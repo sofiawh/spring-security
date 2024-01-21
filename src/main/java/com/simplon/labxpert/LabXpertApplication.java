dba5d523a5a5a0b6409475067415ba7008502a31
ayoub <AITSIAHMAD.AYOUB@ofppt-edu.ma>
2024-01-21 12:40:53 +0100

    some javaDoc

--- src/main/java/com/simplon/labxpert/LabXpertApplication.java
+++ src/main/java/com/simplon/labxpert/LabXpertApplication.java
@@ -3,9 +3,13 @@ package com.simplon.labxpert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
+/**
+ * Main class of the application.
+ * @EnableScheduling is used to enable the scheduling of the tasks like the check of the expiration date of the reagents.
+ * @Author Ayoub Ait Si Ahmad
+ */
 @EnableScheduling
+@SpringBootApplication
 public class LabXpertApplication {
 
     public static void main(String[] args) {
